6366c13677ba72f804d439d24971ae080735cbfd
jansen <Paul.Jansen@tiobe.com>
2026-02-22 18:05:04 +0100

    #37918: Implemented GawlKeeper rule Convention 23.

--- test/all.sh
+++ test/all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for id in $(gk -showrules 2>&1 | cut -d'|' -f 1); do
+gk=../GawlKeeper.linux64
+for id in $($gk -showrules 2>&1 | cut -d'|' -f 1); do
     echo -e " ----- Rule $id ------------------------------------\n";
     for f in $(find ! -type d -name "${id}*"); do
-        echo -e "gk --$id $f\n"
-        gk --$id $f
+        echo -e "$gk --$id $f\n"
+        $gk --$id $f
     done
 done
